11753106abe953b95eb64fb1bf2d65a6930e5571
Intertainment <apple@appledemacbook-pro.local>
2015-10-12 17:52:21 +0800

    Initial Commit

---
+++ BackgroundLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroundLocation
+//
+//  Created by Intertainment on 15/10/12.
+//  Copyright © 2015年 Intertainment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
